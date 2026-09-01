4a8afa297f5a9616f681b678c64936d7648d254a
LizCanfrei <lizziecandidodefreitas@gmail.com>
2026-09-01 16:47:28 -0300

    atualização exercícios While

---
+++ While/ler_nome_nota_calcular_While/ler_nome_nota_calcular_While/Program.cs
@@ -0,0 +1,20 @@
+﻿double cont=0, notaA, notaB, media;
+string nome;
+
+while (cont <= 5)
+{
+    Console.WriteLine("Digite o nome do aluno: ");
+    nome = Console.ReadLine();
+
+    Console.WriteLine("Digite a nota A: ");
+    notaA = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite a nota B: ");
+    notaB = double.Parse(Console.ReadLine());
+
+    media = notaA + notaB / 2;
+
+    Console.WriteLine(nome + " - Tem média: " + media);
+
+    cont++;
+}
---
+++ While/ler_num_comando_ate_10_depois_dividir_por_3/ler_num_comando_ate_10_depois_dividir_por_3/Program.cs
@@ -0,0 +1,12 @@
+﻿double num, cont = 10, divisao;
+
+Console.WriteLine("Digite um número: ");
+num = double.Parse(Console.ReadLine());
+
+while(cont <= num)
+{
+    divisao = cont / 3;
+    Console.WriteLine(divisao);
+
+    cont++;
+}
